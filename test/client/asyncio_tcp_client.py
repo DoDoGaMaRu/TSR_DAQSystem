cce298eb5ab6d0a2588b0e5a49247363f184fed9
wintee <b12e34e56@naver.com>
2023-08-12 18:26:57 +0900

    [목업 : TCP 클라이언트 재연결 구현]

--- test/client/asyncio_tcp_client.py
+++ test/client/asyncio_tcp_client.py
@@ -1,6 +1,7 @@
 import io
 import pickle
 import asyncio
+import random
 from asyncio import transports
 
 HOST = 'localhost'
@@ -8,19 +9,29 @@ PORT = 8082
 
 machine_name = 'test'
 
-# TODO 연결 상태 확인 및 재연결 알고리즘 추가
+
 class TCPClientProtocol(asyncio.Protocol):
+    def __init__(self):
+        self.transport = None
+
     def connection_made(self, transport: transports.WriteTransport) -> None:
         self.transport = transport
         self.transport.write(machine_name.encode() + b'\n')
+        print('connection made')
 
-    def send_data_with_extrainfo(self, data, extrainfo):
+    def send_data_with_extrainfo(self, data, extrainfo) -> None:
         message = f"{data}:{extrainfo}\n".encode()
         self.transport.write(message)
 
-    def send_data(self, event, data):
-        self.transport.write(send_protocol(event=event,
+    def send_data(self, event, data) -> None:
+        if not self.transport.is_closing():
+            self.transport.write(send_protocol(event=event,
                                            data=data))
+        else:
+            raise Exception("Attempted to send data but lost connection")
+
+    def is_closing(self) -> bool:
+        return self.transport.is_closing()
 
     def connection_lost(self, exc) -> None:
         self.transport.close()
@@ -34,21 +45,31 @@ def send_protocol(event, data):
     return serialized + b'\n'
 
 
-async def msg_loop(protocol):
-    while True:
-        msg = input('send : ')
-        protocol.send_data(event='test', data=msg)
-        await asyncio.sleep(0.1)
+async def save_loop(msg: float) -> None:
+    # TODO 데이터 저장 로직 구현
+    print('data save')
+    await asyncio.sleep(1)
+
+
+async def send_loop(protocol: TCPClientProtocol, msg: float) -> None:
+    protocol.send_data(event='test', data=msg)
+    print('data send')
+    await asyncio.sleep(1)
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
+    protocol: TCPClientProtocol = None
 
     while True:
-        try:
-            conn = loop.create_connection(lambda: TCPClientProtocol(), HOST, PORT)
-            protocol: TCPClientProtocol
-            transport, protocol = loop.run_until_complete(conn)
-            loop.run_until_complete(msg_loop(protocol))
-        except Exception:
-            pass
+        msg = random.random()
+
+        if protocol is not None and not protocol.is_closing():
+            loop.run_until_complete(send_loop(protocol, msg))
+        else:
+            try:
+                conn = loop.create_connection(lambda: TCPClientProtocol(), HOST, PORT)
+                result = loop.run_until_complete(asyncio.gather(save_loop(msg), conn))
+                transport, protocol = result[1]
+            except ConnectionRefusedError:
+                pass
